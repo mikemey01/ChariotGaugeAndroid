6771362845f50e545d08846ddaf2e380defb490b
mikemey01 <mikemey01@gmail.com>
2015-05-11 22:37:39 -0600

    added ble to RPM

--- app/src/main/java/com/chariotinstruments/chariotgauge/RPMActivity.java
+++ app/src/main/java/com/chariotinstruments/chariotgauge/RPMActivity.java
@@ -1,13 +1,16 @@
 package com.chariotinstruments.chariotgauge;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.IBinder;
 import android.os.Looper;
 import android.os.Message;
 import android.preference.PreferenceManager;
@@ -36,6 +39,7 @@ public class RPMActivity extends Activity implements Runnable {
     float        voltSValue;
     boolean      paused;
     Thread       thread;
+    boolean      isBLE;
 
 
     //Prefs vars
@@ -59,8 +63,25 @@ public class RPMActivity extends Activity implements Runnable {
     private static final int CURRENT_TOKEN = 5;
     private static final int VOLT_TOKEN    = 0;
 
-    BluetoothSerialService mSerialService; 
+    //Bluetooth types
+    private static final int CLASSIC_TYPE = 1;
+    private static final int BLE_TYPE     = 2;
+
+    BluetoothSerialService mSerialService;
+    BluetoothLeService _bluetoothLeService;
     private static Handler workerHandler;
+
+    //Used for BLE Service life-cycle
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            _bluetoothLeService = ((BluetoothLeService.LocalBinder) service).getService();
+        }
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            _bluetoothLeService = null;
+        }
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -98,15 +119,36 @@ public class RPMActivity extends Activity implements Runnable {
             txtViewDigital.setText(Float.toString(multiGauge.getMinValue()));
         }
 
-        //Get the mSerialService object from the UI activity.
+        //Get the mSerialService/BLE service object from the UI activity.
         Object obj = PassObject.getObject();
+        int _bluetoothType = PassObject.getType();
+
+        isBLE = false;
+        if(_bluetoothType == CLASSIC_TYPE){
+            isBLE = false;
+        }else if(_bluetoothType == BLE_TYPE){
+            isBLE = true;
+        }
+
         //Assign it to global mSerialService variable in this activity.
-        mSerialService = (BluetoothSerialService) obj;
-        
+        if(!isBLE) {
+            mSerialService = (BluetoothSerialService) obj;
+        }else{
+            _bluetoothLeService = (BluetoothLeService) obj;
+        }
+
         //Check if the serial service object is null - assign the handler.
-        if(mSerialService != null){
+        if(mSerialService != null && !isBLE){
             //Update the BluetoothSerialService instance's handler to this activities.
             mSerialService.setHandler(mHandler);
+        }
+
+        if(_bluetoothLeService != null && isBLE){
+            Intent gattServiceIntent = new Intent(this, BluetoothLeService.class);
+            startService(gattServiceIntent);
+            bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
+
+            _bluetoothLeService.setHandler(mHandler);
         }
 
         thread = new Thread(RPMActivity.this);
@@ -206,8 +248,11 @@ public class RPMActivity extends Activity implements Runnable {
 
     @Override
     public void onBackPressed(){
+        if(_bluetoothLeService != null) {
+            unbindService(mServiceConnection);
+        }
         paused = true;
-        //workerHandler.getLooper().quit();
+        passObject();
         super.onBackPressed();
     }
     
@@ -269,5 +314,14 @@ public class RPMActivity extends Activity implements Runnable {
         showVoltMeter = sp.getBoolean("showVoltMeter", true);
     }
 
+    private void passObject(){
+        if(!isBLE){
+            PassObject.setObject(mSerialService);
+            PassObject.setType(1);
+        }else{
+            PassObject.setObject(_bluetoothLeService);
+            PassObject.setType(2);
+        }
+    }
 
 }
